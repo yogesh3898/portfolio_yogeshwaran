4e53c0554bd9ec07c9dfd5c3147f0799d695f147
Yogesh <yogeshwaran3898@gmail.com>
2022-07-09 15:10:22 +0530

    second

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,7 @@ const Contact=()=>{
     return(
         <div className="contact" id="contact">
             <h1>Contact Information</h1>
-            <h2>Phone: 1212121212</h2>
+            <h2>Phone: 121212121254</h2>
             <h2>email: something@something.com</h2>
         </div>
     )
